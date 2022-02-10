fe573bc31661aa85c56dbcfa053ac3dc32530f2b
Ashok30-vk <71430874+Ashok30-vk@users.noreply.github.com>
2022-02-10 14:43:07 +0545

    Add files via upload

--- file_handler.php
+++ file_handler.php
@@ -2,4 +2,18 @@
 $name= $_POST['name'];
 $visitor_email= $_POST['email'];
 $comment= $_POST['comment'];
+
+$email_from='ashokgahatraj.com.np';
+
+$email_subject ='New Form Submission'
+$email_body="User Name: $name.\n".
+             "User Email: $visitor_email.\n"
+             "User Message:$comment.\n";
+
+$to='gahatrajashok125@gmail.com';
+
+$headers="From:$email_from\r\n";
+$headers.="Reply-To:$visitor_email\r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:about.html")
 ?>
